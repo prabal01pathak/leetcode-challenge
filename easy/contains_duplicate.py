e0acd8267f3d9d131954e6ccacc76b57dedf0767
Prabal Pathak <prabal01pathak@gmail.com>
2022-06-03 14:39:37 +0530

    contains_duplicates: use set insted of list

--- easy/contains_duplicate.py
+++ easy/contains_duplicate.py
@@ -8,11 +8,24 @@ class Solution:
             return False
         return True
     
+    def containsDuplicate2(self, nums: List[int]) -> bool:
+        unique = set()
+        index = 0
+        while index < len(nums):
+            if nums[index] not in unique:
+                unique.add(nums[index])
+            else:
+                return True
+            index += 1
+        return False
+    
 
 def test_containsDuplicate():
     c = Solution()
     assert c.containsDuplicate([1,2,3,1]) == True
     assert c.containsDuplicate([1,2,3,4]) == False
+    assert c.containsDuplicate2([1,2,3,1]) == True
+    assert c.containsDuplicate2([1,2,3,4]) == False
 
 
 if __name__ == "__main__":
